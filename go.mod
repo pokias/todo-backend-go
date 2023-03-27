d7d880c7c97ecb886588d72df0ef9512e91061e7
pokias <16976739+pokias@users.noreply.github.com>
2023-03-27 21:11:15 +0300

    db part should work, now to parsin requests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Todo-backend
 go 1.20
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
 	github.com/hashicorp/go-memdb v1.3.4 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
--- todo-backend.go
+++ todo-backend.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/hashicorp/go-memdb"
 )
 
@@ -11,10 +12,12 @@ func noteCallHandler(w http.ResponseWriter, req *http.Request) {
 
 	switch req.Method {
 	case "GET":
-		//handle Gett
+		//handle Get, get All Notes
+		getAllNotes()
 
 	case "POST":
 		//handle POST
+		//createUpdateNote(nil)
 
 	case "DELETE":
 		//Handle deleting note
@@ -32,10 +35,10 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	//initialize db
-	initialize()
 	testAddNotes()
 	testGetNotes()
+	getAllNotes()
+	getAllNotesByPoster(3)
 
 	http.HandleFunc("/note", noteCallHandler)
 	http.HandleFunc("/headers", headers)
@@ -45,47 +48,36 @@ func main() {
 
 type Note struct {
 	Note   string
+	Id     string
 	Poster int
-	Id     int
 }
 
-var db memdb.MemDB
-
-func initialize() {
-
-	// Create the DB schema
-	schema := &memdb.DBSchema{
-		Tables: map[string]*memdb.TableSchema{
-			"note": &memdb.TableSchema{
-				Name: "note",
-				Indexes: map[string]*memdb.IndexSchema{
-					"note": &memdb.IndexSchema{
-						Name:    "note",
-						Unique:  false,
-						Indexer: &memdb.StringFieldIndex{Field: "Note"},
-					},
-					"id": &memdb.IndexSchema{
-						Name:    "id",
-						Unique:  true,
-						Indexer: &memdb.IntFieldIndex{Field: "Id"},
-					},
-					"poster": &memdb.IndexSchema{
-						Name:    "poster",
-						Unique:  false,
-						Indexer: &memdb.IntFieldIndex{Field: "Poster"},
-					},
+// globaali declaraatio?
+var schema = &memdb.DBSchema{
+	Tables: map[string]*memdb.TableSchema{
+		"note": {
+			Name: "note",
+			Indexes: map[string]*memdb.IndexSchema{
+				"note": {
+					Name:    "note",
+					Unique:  false,
+					Indexer: &memdb.StringFieldIndex{Field: "Note"},
+				},
+				"id": {
+					Name:    "id",
+					Unique:  true,
+					Indexer: &memdb.StringFieldIndex{Field: "Id"},
+				},
+				"poster": {
+					Name:    "poster",
+					Unique:  false,
+					Indexer: &memdb.IntFieldIndex{Field: "Poster"},
 				},
 			},
 		},
-	}
-
-	// Create a new data base
-	db, err := memdb.NewMemDB(schema)
-	if err != nil {
-		panic(err)
-	}
-	_ = db
+	},
 }
+var db, err = memdb.NewMemDB(schema)
 
 func testAddNotes() {
 	// Create a write transaction
@@ -93,10 +85,10 @@ func testAddNotes() {
 
 	// Insert some people
 	note := []*Note{
-		{"This is test note number 1", 30, 30},
-		{"This is test note number 2", 31, 35},
-		{"This is test note number 3", 32, 21},
-		{"This is test note number 4", 33, 53},
+		{"This is test note number 1", "f543dd77-826f-4809-a42c-174ac3e5b140", 30},
+		{"This is test note number 2", "d4ad9db6-5635-44f9-ad75-cae291bb21b4", 35},
+		{"This is test note number 3", "da85f4f2-cdd2-4190-a530-be12e425cd44", 21},
+		{"This is test note number 4", "7169a2fe-cc09-11ed-afa1-0242ac120002", 53},
 	}
 	for _, p := range note {
 		if err := txn.Insert("note", p); err != nil {
@@ -107,6 +99,64 @@ func testAddNotes() {
 	txn.Commit()
 }
 
+func getAllNotes() []Note {
+	// Create read-only transaction
+	txn := db.Txn(false)
+	defer txn.Abort()
+
+	// query all the people
+	it, err := txn.Get("note", "note")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("All the notes:")
+	AllNotesArray := make([]Note, 0, 1)
+	for obj := it.Next(); obj != nil; obj = it.Next() {
+		p := obj.(*Note)
+		AllNotesArray = append(AllNotesArray, *p)
+		fmt.Printf("  %s note with poster %d\n", p.Note, p.Poster)
+	}
+	return AllNotesArray
+}
+
+func getAllNotesByPoster(posterId int) []Note {
+	// Create read-only transaction
+	txn := db.Txn(false)
+	defer txn.Abort()
+
+	// query all the people
+	it, err := txn.Get("note", "poster", posterId)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("All the notes by poster:")
+	AllNotesArray := make([]Note, 0, 1)
+	for obj := it.Next(); obj != nil; obj = it.Next() {
+		p := obj.(*Note)
+		AllNotesArray = append(AllNotesArray, *p)
+		fmt.Printf("  %s note with poster %d\n", p.Note, p.Poster)
+	}
+	return AllNotesArray
+}
+
+func createUpdateNote(note Note) {
+
+	txn := db.Txn(true)
+	if note.Id != "" {
+		note.Id = uuid.New().String()
+	}
+	if err := txn.Insert("note", note); err != nil {
+		panic(err)
+	}
+}
+
+func deleteNote(uuid string) {
+	txn := db.Txn(true)
+	txn.Delete("note", Note{Id: uuid})
+}
+
 func testGetNotes() {
 
 	// Create read-only transaction
@@ -114,7 +164,7 @@ func testGetNotes() {
 	defer txn.Abort()
 
 	// Lookup by id
-	raw, err := txn.First("note", "id", 53)
+	raw, err := txn.First("note", "id", "f543dd77-826f-4809-a42c-174ac3e5b140")
 	if err != nil {
 		panic(err)
 	}
@@ -122,8 +172,10 @@ func testGetNotes() {
 	// Say hi!
 	fmt.Printf("Hello %s!\n", raw.(*Note).Note)
 
+	deleteNote(raw.(*Note).Id)
+
 	// List all the notes
-	it, err := txn.Get("note", "poster", "id")
+	it, err := txn.Get("note", "poster", 30)
 	if err != nil {
 		panic(err)
 	}
@@ -133,29 +185,4 @@ func testGetNotes() {
 		p := obj.(*Note)
 		fmt.Printf("  %s\n", p.Note)
 	}
-
-	// Range scan over people with ages between 25 and 35 inclusive
-	it, err = txn.LowerBound("note", "poster", 25)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("People aged 25 - 35:")
-	for obj := it.Next(); obj != nil; obj = it.Next() {
-		p := obj.(*Note)
-		if p.Id > 35 {
-			break
-		}
-		fmt.Printf("  %s is aged %d\n", p.Note, p.Poster)
-	}
-	// Output:
-	// Hello Joe!
-	// All the people:
-	//   Dorothy
-	//   Joe
-	//   Lucy
-	//   Tariq
-	// People aged 25 - 35:
-	//   Joe is aged 30
-	//   Lucy is aged 35
 }
